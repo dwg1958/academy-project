537363d09babb463ac42e322fd095fd47d6af139
DavidG <david@w5h.com>
2021-03-02 19:01:25 +0000

    All 4 formulas working and team name edit

--- season/views.py
+++ season/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from .models import Competitor, Event, ScoringEvent, Result, TeamProfile
 from django.contrib.auth.decorators import login_required
-from .forms import EventForm, TeamProfileForm
+from .forms import EventForm, TeamProfileForm, TeamProfileForm
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -118,33 +118,82 @@ def teamview(request):
             returnURL = 'season/teamview.html?tab=F1'
 
         elif 'F2_submit' in request.POST:
-            print('F2 submit found')
-            print('p2_1 =',request.POST['p2_1'])
-            print('p2_2 =',request.POST['p2_2'])
-            recordtoedit.p2_1 = Competitor.objects.get(pk=request.POST['p2_1'])
-            recordtoedit.p2_2 = Competitor.objects.get(pk=request.POST['p2_2'])
-            #Save data to table
-            recordtoedit.save()
+            # POST arg comes in as value="1|Lewis Hamilton|29.0"
+            arg1 = request.POST['p2_1_picker'] #Get arg
+            arg1 = arg1[:arg1.index("|")]      #Split out the first element - the driverID
+            arg2 = request.POST['p2_2_picker']
+            arg2 = arg2[:arg2.index("|")]
+
+            new_p2_1 = Competitor.objects.get(pk=arg1) #Find new driver 2 record
+            new_p2_2 = Competitor.objects.get(pk=arg2)
+            #check budget not exceeded
+            new_cash_pot = recordtoedit.f2_cashpot + recordtoedit.p2_1.value + recordtoedit.p2_2.value - new_p2_1.value - new_p2_2.value
+
+            if ( new_cash_pot < 0 ):
+                returnmessage = 'You have exceeded your budget - please choose again'
+            elif ( arg1 == arg2 ):
+                returnmessage = 'You have chosen the same driver twice - please choose again'
+            else:
+                recordtoedit.p2_1 = new_p2_1
+                recordtoedit.p2_1_cost = new_p2_1.value
+                recordtoedit.p2_2 = new_p2_2
+                recordtoedit.p2_2_cost = new_p2_2.value
+                recordtoedit.f2_cashpot = new_cash_pot
+                #Save data to table
+                recordtoedit.save()
+
             returnURL = 'season/teamview.html?tab=F2'
 
         elif 'F3_submit' in request.POST:
-            print('F3 submit found')
-            print('p3_1 =',request.POST['p3_1'])
-            print('p3_2 =',request.POST['p3_2'])
-            recordtoedit.p3_1 = Competitor.objects.get(pk=request.POST['p3_1'])
-            recordtoedit.p3_2 = Competitor.objects.get(pk=request.POST['p3_2'])
-            #Save data to table
-            recordtoedit.save()
+            arg1 = request.POST['p3_1_picker'] #Get arg
+            arg1 = arg1[:arg1.index("|")]      #Split out the first element - the driverID
+            arg2 = request.POST['p3_2_picker']
+            arg2 = arg2[:arg2.index("|")]
+
+            new_p3_1 = Competitor.objects.get(pk=arg1) #Find new driver 2 record
+            new_p3_2 = Competitor.objects.get(pk=arg2)
+            #check budget not exceeded
+            new_cash_pot = recordtoedit.f3_cashpot + recordtoedit.p3_1.value + recordtoedit.p3_2.value - new_p3_1.value - new_p3_2.value
+
+            if ( new_cash_pot < 0 ):
+                returnmessage = 'You have exceeded your budget - please choose again'
+            elif ( arg1 == arg2 ):
+                returnmessage = 'You have chosen the same driver twice - please choose again'
+            else:
+                recordtoedit.p3_1 = new_p3_1
+                recordtoedit.p3_1_cost = new_p3_1.value
+                recordtoedit.p3_2 = new_p3_2
+                recordtoedit.p3_2_cost = new_p3_2.value
+                recordtoedit.f3_cashpot = new_cash_pot
+                #Save data to table
+                recordtoedit.save()
+
             returnURL = 'season/teamview.html?tab=F3'
 
         elif 'WS_submit' in request.POST:
-            print('WS submit found')
-            print('pw_1 =',request.POST['pw_1'])
-            print('pw_2 =',request.POST['pw_2'])
-            recordtoedit.pw_1 = Competitor.objects.get(pk=request.POST['pw_1'])
-            recordtoedit.pw_2 = Competitor.objects.get(pk=request.POST['pw_2'])
-            #Save data to table
-            recordtoedit.save()
+            arg1 = request.POST['pw_1_picker'] #Get arg
+            arg1 = arg1[:arg1.index("|")]      #Split out the first element - the driverID
+            arg2 = request.POST['pw_2_picker']
+            arg2 = arg2[:arg2.index("|")]
+
+            new_pw_1 = Competitor.objects.get(pk=arg1) #Find new driver 2 record
+            new_pw_2 = Competitor.objects.get(pk=arg2)
+            #check budget not exceeded
+            new_cash_pot = recordtoedit.ws_cashpot + recordtoedit.pw_1.value + recordtoedit.pw_2.value - new_pw_1.value - new_pw_2.value
+
+            if ( new_cash_pot < 0 ):
+                returnmessage = 'You have exceeded your budget - please choose again'
+            elif ( arg1 == arg2 ):
+                returnmessage = 'You have chosen the same driver twice - please choose again'
+            else:
+                recordtoedit.pw_1 = new_pw_1
+                recordtoedit.pw_1_cost = new_pw_1.value
+                recordtoedit.pw_2 = new_pw_2
+                recordtoedit.pw_2_cost = new_pw_2.value
+                recordtoedit.ws_cashpot = new_cash_pot
+                #Save data to table
+                recordtoedit.save()
+
             returnURL = 'season/teamview.html?tab=WS'
 
     else:
@@ -159,14 +208,20 @@ def teamview(request):
     return render(request, 'season/teamview.html',{ 'returnmessage':returnmessage, 'teamdata':teamdata, 'f1drivers':f1drivers, 'f2drivers':f2drivers, 'f3drivers':f3drivers, 'wsdrivers':wsdrivers})
 
 
-
-
-
-
 @login_required
 ### EDIT MY TEAM #####################
-def teampicker(request):
-    return render(request, 'season/teampicker.html')
+def teamnamepicker(request):
+    if request.method == 'POST':
+        team_form = TeamProfileForm(
+                                    instance=request.user.team,
+                                    data=request.POST,
+                                    files=request.FILES)
+        if team_form.is_valid():
+            team_form.save()
+            return redirect('teamnamepicker')
+    else:
+        team_form = TeamProfileForm(instance=request.user.team)
+    return render(request,'season/teamnamepicker.html', {'team_form': team_form})
 
 ############################### ADMIN PAGES ###################################################
 
